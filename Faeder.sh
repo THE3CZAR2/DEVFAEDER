98b63b562ea71d9fc4e884f4e47cdd6498211669
hackr2009 <omarteam737@gmail.com>
2017-11-19 12:52:38 +0300

    Update Faeder.sh

--- Faeder.sh
+++ Faeder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 token="399096444:AAHav14judfKas9nU1lKGMa9pOxvRS84BrY"
 function print_logo() {
-	echo -e "\e[38;5;77m"   
+ echo -e "\e[38;5;77m"   
 echo -e 
 echo -e 
 echo -e 
@@ -57,5 +57,5 @@ echo -e
 echo -e 
 echo -e    
 
-curl "https://api.telegram.org/bot"$token"/sendmessage" -F
-./tg -s ./PAIN.lua $@ --bot=$token
+curl "https://core.telegram.org/bot"$token"/sendmessage" -F
+./tg -s ./FAEDER.lua $@ --bot=$token
