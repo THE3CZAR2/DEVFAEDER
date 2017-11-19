419ed859df0bc42370c8ac0ce5f6536f0c0938f5
hackr2009 <omarteam737@gmail.com>
2017-11-19 13:36:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
 
+cd $HOME/DEVFAEDER
+token="TOKEN"
+install() {
+ 
 sudo apt-get update
 
 sudo apt-get upgrade -y
@@ -45,10 +50,66 @@ wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  sudo easy_install pip
  sudo pip install redis
  cd ..
- cd DEVFAEDER
+ cd DEVFAEDER 
  rm -rf "luarocks-2.2.2"
  wget "https://valtman.name/files/telegram-cli-1222"
  mv telegram-cli-1222 tg
  chmod +x tg
- chmod +x faeder.sh
- ./faeder.sh
+ chmod +x Faeder.sh
+ ./Faeder.sh
+
+}
+
+if [ "$1" = "install" ]; then
+  install
+  else
+
+if [ ! -f ./tg ]; then
+    echo "tg not found"
+    echo "Run $0 install"
+    exit 1
+ fi
+if [ ! $token ]; then
+  echo -e "\e[1;36mToken Not found\e[0m"
+ exit 1
+ fi
+   curl "https://core.telegram.org/bot"$token"/sendmessage" -F
+   ./tg -s ./FAEDER.lua $@ --bot=$token
+
+fi
+
+# Now All Argument Support after ./STORM.sh !
+# Arguments :
+#   #   #   #   #   #   #   #   #   #
+#  --phone/-u                           specify username (would not be asked during authorization)
+#  --verbosity/-v                       increase verbosity (0-ERROR 1-WARNIN 2-NOTICE 3+-DEBUG-levels)
+#  --enable-msg-id/-N                   message num mode
+#  --config/-c                          config file name
+#  --profile/-p                         use specified profile
+#  --wait-dialog-list/-W                send dialog_list query and wait for answer before reading input
+#  --disable-colors/-C                  disable color output
+#  --disable-readline/-R                disable readline
+#  --alert/-A                           enable bell notifications
+#  --daemonize/-d                       daemon mode
+#  --logname/-L <log-name>              log file name
+#  --username/-U <user-name>            change uid after start
+#  --groupname/-G <group-name>          change gid after start
+#  --disable-output/-D                  disable output
+#  --tcp-port/-P <port>                 port to listen for input commands
+#  --udp-socket/-S <socket-name>        unix socket to create
+#  --exec/-e <commands>                 make commands end exit
+#  --disable-names/-I                   use user and chat IDs in updates instead of names
+#  --help/-h                            prints this help
+#  --accept-any-tcp                     accepts tcp connections from any src (only loopback by default)
+#  --disable-link-preview               disables server-side previews to links
+#  --bot/-b                             bot mode
+#  --json                               prints answers and values in json format
+#  --permanent-msg-ids                  use permanent msg ids
+#  --permanent-peer-ids                 use permanent peer ids
+#   #   #   #   #   #   #   #   #   #
+#Example To launch with second profile :
+# ./Faeder.sh -p second-profile
+
+#     OR
+
+# ./Faeder.sh --profile second-profile
