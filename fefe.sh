7cdf325c0af515d3508c78b535371f4b437070ab
hackr2009 <omarteam737@gmail.com>
2017-11-25 14:41:48 +0300

    Update fefe.sh

--- fefe.sh
+++ fefe.sh
@@ -1,71 +1,41 @@
-#!/usr/bin/env bash
-
-cd $HOME/STORM
-token_="351460214:AAHEXlncjPyhF22zlKToJa6fAGQlnJU8D6E" 
-install() {
-	    cd tg
-		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-		sudo apt-get install g++-4.7 -y c++-4.7 -y
-		sudo apt-get update
-		sudo apt-get upgrade
-		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
-		sudo apt-get install screen -y
-		sudo apt-get install tmux -y
-		sudo apt-get install libstdc++6 -y
-		sudo apt-get install lua-lgi -y
-		sudo apt-get install libnotify-dev -y
-		sudo service redis-server restart
-		wget https://valtman.name/files/telegram-cli-1222
-		mv telegram-cli-1222 tgcli
-		chmod +x tgcli
-		cd ..
-		chmod +x bot
-		chmod +x tg
-}
-
+#!/bin/bash 
+ COUNTER=0
+ token_="351460214:AAHEXlncjPyhF22zlKToJa6fAGQlnJU8D6E" 
+while [  $COUNTER -lt 5 ]; do
+kill $(pgrep telegram-cli)
 function print_logo() {
-	echo -e "
- __   ___________   ___     _____     __  __
-/  |  |___   ___|  / _ \   | ___ }   |  \/  |
-\_ \      | |     | | | |  | |_) }   | |\/| |
- _) |     | |     | |_| |  |  _< \   | |  | |
-|__/      |_|      \___/   |_|  \_\  |_|  |_|
- BY @TAHAJ20 DEV : TAHAJ20            "
-	echo -e " "
-	echo -e " "
-	echo -e " "
-	echo -e " "
-	echo -e " "
+	echo -e "\e[38;5;77m"   
+echo -e "       CH > @TshAkETEAM            "
+echo -e "       CH > @TshAkETEAM           "
+echo -e "       CH > @TshAkETEAM    "
+echo -e "       CH > @TshAkETEAM     "
+echo -e "       CH > @TshAkETEAM          \e[38;5;88m"
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e "\e[33m        _____    _        _    _    _____    Dev @lIMyIl\e[0m"
+echo -e "\e[33m       |_   _|__| |__    / \  | | _| ____|   Dev @EMADOFFICAL\e[0m"
+echo -e "\e[33m         | |/ __| '_ \  / _ \ | |/ /  _|     Dev @IX00XI\e[0m"
+echo -e "\e[33m         | |\__ \ | | |/ ___ \|   <| |___    Dev @H_173\e[0m"
+echo -e "\e[33m         |_||___/_| |_/_/   \_\_|\_\_____|   Dev @lIESIl\e[0m"
+echo -e "\e[33m                                             Dev @h_k_a\e[0m"
 }
-
-function logo_play() {
-    declare -A txtlogo
-    seconds=" "
-    txtlogo[1]=" "
-    txtlogo[2]=" "
-    txtlogo[3]=" "
-    txtlogo[4]=" "
-    printf "\033[38;5;600m\t"
-    for i in ${!txtlogo[@]}; do
-        for x in `seq 0 ${#txtlogo[$i]}`; do
-            printf "${txtlogo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-    printf "\n"
-}
-
-if [ "$1" = "install" ]; then
-  install
-  else
-
+sleep 2
 if [ ! -f ./tg ]; then
-    echo "tg not found"
+echo -e ""
+echo -e "\e[33m        _____    _        _    _    _____    Dev @lIMyIl"
+echo -e "\e[33m       |_   _|__| |__    / \  | | _| ____|   Dev @EMADOFFICAL"
+echo -e "\e[33m         | |/ __| '_ \  / _ \ | |/ /  _|     Dev @IX00XI"
+echo -e "\e[33m         | |\__ \ | | |/ ___ \|   <| |___    Dev @H_173"
+echo -e "\e[33m         |_||___/_| |_/_/   \_\_|\_\_____|   Dev @lIESIl"
+echo -e "\e[33m                                             Dev @h_k_a"
+    echo "tg لا يوجد ملف"
     echo "Run $0 install"
     exit 1
- fi
+    fi
+
 if [ ! $token_ ]; then
+
 echo -e ""
 
 echo -e ""
@@ -75,27 +45,21 @@ echo -e ""
  exit 1
 
  fi
-   print_logo
-   echo -e "\033[38;5;208m"
-   echo -e "
- __   ___________   ___     _____     __  __
-/  |  |___   ___|  / _ \   | ___ }   |  \/  |
-\_ \      | |     | | | |  | |_) }   | |\/| |
- _) |     | |     | |_| |  |  _< \   | |  | |
-|__/      |_|      \___/   |_|  \_\  |_|  |_|
- BY @TAHAJ20 DEV : TAHAJ20            
-"
-   echo -e "    "
-   echo -e "   "
-   echo -e "   "
-   echo -e "     "
-   echo -e "\033[0;00m"
-   echo -e "\e[36m"
-   logo_play
-   #sudo service redis-server restart
-   #./tg -s ./FAEDER.lua -l 1 -E $@ --bot=$token_
+ sleep 2
+  print_logo
+   echo -e ""
+echo -e ""
+echo -e "        \e[38;5;300mOperation | Starting Bot"
+echo -e "        Source | TSHAKE Version 28 March 2017"
+echo -e "        CH  | @TshAkETEAM"
+echo -e "        Dev | @lIMyIl"
+echo -e "        Dev | @IX00XI"
+echo -e "        Dev | @lIESIl"
+echo -e "        Dev | @H_173"
+echo -e "        Dev | @EMADOFFICAL"
+echo -e "        Dev | @h_k_a"
+echo -e "        \e[38;5;40m"
 curl "https://api.telegram.org/bot"$token_"/sendmessage" -F
-
-./tg -s ./FAEDER.lua $@ --bot=$token
-
-fi
+   ./tg -s ./FAEDER.lua $@ --bot=$token_
+sleep 3
+done
